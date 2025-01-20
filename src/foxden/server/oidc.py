807b447b9f92dc42d482b1f02f1bedb9e6fb706a
Zeev Rotshtein <zeev.rotshtein@perception-point.io>
2025-01-20 12:24:35 +0000

    wip: oidc http cache

--- src/foxden/server/oidc.py
+++ src/foxden/server/oidc.py
@@ -1,10 +1,11 @@
 import logging
 import time
 from typing import Annotated, Any, Literal
-from urllib.request import urlopen
 import uuid
 
 from fastapi import Body, HTTPException
+import hishel
+import httpx
 import jwt
 import msgspec
 
@@ -13,6 +14,20 @@ from foxden.server import app
 
 
 logger = logging.getLogger(__name__)
+
+
+class HttpxPyJWKClient(jwt.PyJWKClient):
+    def __init__(self, client: httpx.Client, uri: str, headers: dict[str, Any] | None = None) -> None:
+        super().__init__(uri, cache_keys=False, cache_jwk_set=False, headers=headers)
+        self.client = client
+
+    def fetch_data(self) -> Any:  # noqa: ANN401
+        try:
+            response = self.client.get(self.uri, headers=self.headers)
+            response.raise_for_status()
+            return msgspec.json.decode(response.read())
+        except (httpx.HTTPError, TimeoutError, msgspec.DecodeError) as e:
+            raise jwt.PyJWKClientConnectionError(f'Fail to fetch data from the url, err: "{e}"') from e
 
 
 def custom_jwt_verifier(claims: dict[str, Any]) -> bool:  # TODO: Configuration (how?)
@@ -29,9 +44,10 @@ def oidc_mint_token(token: Annotated[str, Body(embed=True)]) -> dict[Literal['to
     issuer = jwt.decode(token, options={'verify_signature': False})['iss']
     if issuer not in settings.oidc_trusted_issuers:
         raise HTTPException(401)
-    oidc_config = msgspec.json.decode(urlopen(f'{issuer}/.well-known/openid-configuration').read())  # noqa: S310  # TODO: cache
-    jwks_client = jwt.PyJWKClient(oidc_config['jwks_uri'])
-    signing_key = jwks_client.get_signing_key_from_jwt(token)
+    with hishel.CacheClient() as client:
+        oidc_config = msgspec.json.decode(client.get(f'{issuer}/.well-known/openid-configuration').read())
+        jwks_client = HttpxPyJWKClient(client, oidc_config['jwks_uri'])
+        signing_key = jwks_client.get_signing_key_from_jwt(token)
     claims = jwt.decode(token, key=signing_key, audience=settings.oidc_audience)
     if not custom_jwt_verifier(claims):
         raise HTTPException(401)
