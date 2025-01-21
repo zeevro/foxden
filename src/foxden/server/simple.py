062eabde21ff3d174152723d46248bd76ecaa360
Zeev Rotshtein <zeevro@gmail.com>
2025-01-21 17:18:05 +0200

    wip: simple format query parameter

--- src/foxden/server/simple.py
+++ src/foxden/server/simple.py
@@ -1,8 +1,9 @@
 from collections.abc import Iterable
 import contextlib
 from typing import Annotated, Literal, Protocol
+from urllib.parse import quote, quote_plus
 
-from fastapi import Header, HTTPException
+from fastapi import Depends, Header, HTTPException, Query, Request
 from fastapi.responses import Response
 from fastapi.staticfiles import StaticFiles
 
@@ -19,34 +20,45 @@ class IndexResponseModule(Protocol):
     def generate_project_index(self, project: str, files: Iterable[DistFile], url_prefix: str = '') -> bytes | str: ...
 
 
-def resp_format(accept_hdr: str | None) -> Literal['html', 'json']:
-    if accept_hdr:
-        with contextlib.suppress(Exception):
-            for i in accepted_types(accept_hdr):
-                if i.split(';', 1)[0] in 'application/vnd.pypi.simple.v1+json':
-                    return 'json'
-    return 'html'
-
-
-RESP_PARAMS: dict[str, tuple[IndexResponseModule, str]] = {
-    'json': (pep691, 'application/vnd.pypi.simple.v1+json'),
-    'html': (pep503, 'text/html'),
+INDEX_MODULE: dict[str, IndexResponseModule] = {
+    'application/vnd.pypi.simple.v1+json': pep691,
+    'application/vnd.pypi.simple.v1+html': pep503,
+    'text/html': pep503,
 }
+
+DEFAULT_CONTENT_TYPE: str = 'text/html'
+
+DEFAULT_INDEX_MODULE = INDEX_MODULE[DEFAULT_CONTENT_TYPE]
+
+type ReponseParams = tuple[str, IndexResponseModule]
+
+
+def response_params(accept_hdr: Annotated[str | None, Header(alias='accept')] = None, format_param: Annotated[str | None, Query(alias='format')] = None) -> ReponseParams:
+    if format_param:
+        format_param_quoted = quote_plus(format_param, safe='/')
+        if format_param_quoted in INDEX_MODULE:
+            return format_param_quoted, INDEX_MODULE[format_param_quoted]
+    if accept_hdr:
+        for i in accepted_types(accept_hdr):
+            t = i.split(';', 1)[0]
+            with contextlib.suppress(KeyError):
+                return t, INDEX_MODULE[t]  # type: ignore[index]
+    return DEFAULT_CONTENT_TYPE, DEFAULT_INDEX_MODULE
 
 
 @app.get('/')
-def root_index(accept: Annotated[str | None, Header()] = None) -> Response:
-    mod, content_type = RESP_PARAMS[resp_format(accept)]
-    return Response(mod.generate_root_index(settings.index_backend.list_projects()), media_type=content_type)
+def root_index(response_params: Annotated[ReponseParams, Depends(response_params)]) -> Response:
+    content_type, index_module = response_params
+    return Response(index_module.generate_root_index(settings.index_backend.list_projects()), media_type=content_type)
 
 
 @app.get('/{project}/')
-def project_index(project: str, accept: Annotated[str | None, Header()] = None) -> Response:
+def project_index(project: str, response_params: Annotated[ReponseParams, Depends(response_params)]) -> Response:
+    content_type, index_module = response_params
     files = settings.index_backend.files(project)
     if not files:
         raise HTTPException(404)
-    mod, content_type = RESP_PARAMS[resp_format(accept)]
-    return Response(mod.generate_project_index(project, files, '/files/'), media_type=content_type)
+    return Response(index_module.generate_project_index(project, files, '/files/'), media_type=content_type)
 
 
 if hasattr(settings.index_backend, 'dir_path'):
